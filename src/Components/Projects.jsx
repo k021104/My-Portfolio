095c7c83109a23af012244b1b7fd7a7bafc427e3
k021104 <krishnachavda1104@gmail.com>
2026-03-07 23:04:31 +0530

    fix deploy issue

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { Github, ExternalLink } from 'lucide-react'
-import '../styles//Projects.css'
+import '../styles/Projects.css'
 
 const projects = [
   {
